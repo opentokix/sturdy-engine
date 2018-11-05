0b39bb8f723c7011ee5736824378af923ed97d53
OpenTokix <petereriksson.net@gmail.com>
2018-11-05 09:10:50 +0100

    Docker build

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.ListenAndServe(":8080", nil)
 }
