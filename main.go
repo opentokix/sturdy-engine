6a93026d44ebe089bee8cb718a21cc27e0edcec3
OpenTokix <petereriksson.net@gmail.com>
2018-11-11 10:42:19 +0100

    commit

--- main.go
+++ main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	var ver string
+	ver = "1.6"
+	name, _ := os.Hostname()
+	fmt.Fprintf(w, "<html><head><title>Test site</title></head>")
+	fmt.Fprintf(w, "<h1>This is a simple golang test app</h1>")
+	fmt.Fprintf(w, "<p>Server by hostname: %s</p>", name)
+	fmt.Fprintf(w, "<p>Version: %s</p>", ver)
+	fmt.Fprintf(w, "<img src=\"img/gradient.png\">")
+	fmt.Fprintf(w, "</body></html>")
 }
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+	images := http.FileServer(http.Dir("./img"))
+	http.HandleFunc("/", handler)
+	http.Handle("/img/", http.StripPrefix("/img/", images))
 	http.ListenAndServe(":8080", nil)
 }
